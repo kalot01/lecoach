00769dd834b1e2690701ef63ca13d2daf947ccc3
ghachem eya <ghachemeya12@gmail.com>
2021-06-20 21:56:27 +0100

    Section partenaire with image

--- src/components/partenaires.jsx
+++ src/components/partenaires.jsx
@@ -8,9 +8,9 @@ export const Partenaires = (props) => {
           <div className='row'>
             {props.data
               ? props.data.map((d, i) => (
-                  <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3'>
+                  <div key={`${d.name}-${i}`} className='col-xs-6 col-md-3'>
                     {' '}
-                    <i className={d.icon}></i>
+                    <img src={d.img} alt='...' className='team-img' />
                   </div>
                 ))
               : 'Loading...'}
